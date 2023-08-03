cbaa7569ce620cf0836dd4f987f7d021f48fc193
itsshreshth <20bee069@nith.ac.in>
2023-08-04 03:23:58 +0530

    updar

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Footer.scss";
+
+const Footer = () => {
+  return <div className="footer"> &copy; 2023 Travel Agency. All rights reserved.</div>;
+};
+
+export default Footer;
